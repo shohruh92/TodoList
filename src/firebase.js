01c6cda72a03a91643aec4b22e7f90b5b4a48310
shohruh92 <135565751+shohruh92@users.noreply.github.com>
2023-07-03 17:38:44 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCP_CQC4fHfCuVCf33o9Wum968O08KJkHg",
-  authDomain: "myaplication-7a993.firebaseapp.com",
-  databaseURL: "https://myaplication-7a993-default-rtdb.firebaseio.com",
-  projectId: "myaplication-7a993",
-  storageBucket: "myaplication-7a993.appspot.com",
-  messagingSenderId: "16005049020",
-  appId: "1:16005049020:web:82576e28107ccdf468feb9",
-  measurementId: "G-Q4VNQ68PZT"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "3",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
